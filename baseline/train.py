7a9685d45be8be5c44ce7207e3b60c0f0c8f33aa
Mizuki Sango <msageha@yahoo.co.jp>
2018-12-19 04:03:46 +0900

    bag fixed

--- baseline/train.py
+++ baseline/train.py
@@ -179,17 +179,20 @@ def calculate_confusion_matrix(confusion_matrix, _batch, _pred, target_case):
     else:
         target_verb_index = _batch[1].name
         verb_phrase_number = _batch[0]['n文節目'][target_verb_index]
-        pred_dependency_relation_phrase_number = _batch[0]['係り先文節'][_pred]
-        if verb_phrase_number == pred_dependency_relation_phrase_number:
-            pred_case_type = 'intra(dep)'
+        if _pred >= len(_batch[0]['係り先文節']):
+            pred_case_type = 'iner(zero)'
         else:
-            if 'n文目' in _batch[0].keys():
-                if _batch[0]['n文目'][target_verb_index] == _batch[0]['n文目'][_pred]:
-                    pred_case_type = 'intra(zero)'
-                else:
-                    pred_case_type = 'inter(zero)'
+            pred_dependency_relation_phrase_number = _batch[0]['係り先文節'][_pred]
+            if verb_phrase_number == pred_dependency_relation_phrase_number:
+                pred_case_type = 'intra(dep)'
             else:
-                pred_case_type = 'intra(zero)'
+                if 'n文目' in _batch[0].keys():
+                    if _batch[0]['n文目'][target_verb_index] == _batch[0]['n文目'][_pred]:
+                    pred_case_type = 'intra(zero)'
+                    else:
+                        pred_case_type = 'inter(zero)'
+                else:
+                    pred_case_type = 'intra(zero)'
     confusion_matrix['actual'][actual_case_type]['predicted'][pred_case_type] += 1
 
 def test(tests, bilstm, args):
