2bc86a0391de6d9ee9265c3f1184ac1e6f71a7b9
Mizuki Sango <msageha@yahoo.co.jp>
2018-12-22 18:28:13 +0900

    edit shell script

--- WordEmbedAnalysis/train.py
+++ WordEmbedAnalysis/train.py
@@ -33,7 +33,7 @@ def weights_init(m):
 def create_arg_parser():
     parser = argparse.ArgumentParser(description='main function parser')
     parser.add_argument('--type', dest='dataset_type', required=True, choices=['intra', 'inter'], help='dataset: "intra" or "inter"')
-    parser.add_argument('--epochs', '-e', dest='max_epoch', type=int, default=20, help='max epoch')
+    parser.add_argument('--epochs', '-e', dest='max_epoch', type=int, default=15, help='max epoch')
     parser.add_argument('--emb_type', dest='emb_type', required=True, choices=['Word2Vec', 'Word2VecWiki','FastText', 'ELMo', 'Random', 'ELMoForManyLangs', 'None'], help='word embedding type')
     parser.add_argument('--emb_path', dest='emb_path', help='word embedding path')
     parser.add_argument('--emb_requires_grad_false', dest='emb_requires_grad', action='store_false', help='fixed word embedding or not')
