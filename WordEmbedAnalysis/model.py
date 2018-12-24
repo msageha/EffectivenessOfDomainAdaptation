33f36c97162f9461c1b3e448d109633e1c534dbe
Mizuki Sango <msageha@yahoo.co.jp>
2018-12-24 17:06:01 +0900

    modify for pep8

--- WordEmbedAnalysis/model.py
+++ WordEmbedAnalysis/model.py
@@ -4,20 +4,22 @@ import torch.nn.functional as F
 from torch.autograd import Variable
 from allennlp.modules.elmo import Elmo, batch_to_ids
 
+
 class BiLSTM(nn.Module):
-    def __init__(self, emb_dim, h_dim, n_labels, v_size, gpu=True, v_vec=None, batch_first=True, emb_type=None, elmo_model_dir=None):
+    def __init__(self, emb_dim, h_dim, n_labels, v_size,
+            gpu=True, v_vec=None, batch_first=True, emb_type=None, elmo_model_dir=None):
         super(BiLSTM, self).__init__()
         self.gpu = gpu
         self.h_dim = h_dim
-        if self.h_dim == None:
-            self.h_dim = emb_dim+36
-        if emb_type=='ELMo':
+        if self.h_dim is None:
+            self.h_dim = emb_dim + 36
+        if emb_type == 'ELMo':
             options_file = f'{elmo_model_dir}/options.json'
             weight_file = f'{elmo_model_dir}/weights.hdf5'
             self.word_embed = Elmo(options_file, weight_file, num_output_representations=1, dropout=0)
             if gpu:
                 self.word_embed = self.word_embed.cuda()
-        elif emb_type=='ELMoForManyLangs':
+        elif emb_type == 'ELMoForManyLangs':
             from elmoformanylangs import Embedder
             e = Embedder(elmo_model_dir)
             self.word_embed = e.sents2elmo
@@ -31,12 +33,12 @@ class BiLSTM(nn.Module):
 
         feature_embed_layers = []
         feature_embed_size = {
-            "feature:0" : 25,
-            "feature:1" : 26,
-            "feature:2" : 12,
-            "feature:3" : 6,
-            "feature:4" : 94,
-            "feature:5" : 32
+            "feature:0": 25,
+            "feature:1": 26,
+            "feature:2": 12,
+            "feature:3": 6,
+            "feature:4": 94,
+            "feature:5": 32
         }
         for key in feature_embed_size:
             size = feature_embed_size[key]
@@ -45,12 +47,12 @@ class BiLSTM(nn.Module):
             feature_embed_layers.append(feature_embed)
         self.feature_embed_layers = nn.ModuleList(feature_embed_layers)
         self.drop_target = nn.Dropout(p=0.2)
-        self.lstm = nn.LSTM(input_size=emb_dim+36, hidden_size=self.h_dim, batch_first=batch_first, bidirectional=True)
-        self.l1 = nn.Linear(self.h_dim*2, n_labels)
+        self.lstm = nn.LSTM(input_size=emb_dim + 36, hidden_size=self.h_dim, batch_first=batch_first, bidirectional=True)
+        self.l1 = nn.Linear(self.h_dim * 2, n_labels)
 
     def init_hidden(self, b_size):
-        h0 = Variable(torch.zeros(1*2, b_size, self.h_dim))
-        c0 = Variable(torch.zeros(1*2, b_size, self.h_dim))
+        h0 = Variable(torch.zeros(1 * 2, b_size, self.h_dim))
+        c0 = Variable(torch.zeros(1 * 2, b_size, self.h_dim))
         if self.gpu:
             h0 = h0.cuda()
             c0 = c0.cuda()
@@ -110,6 +112,6 @@ class BiLSTM(nn.Module):
         x = self.drop_target(x)
         out, hidden = self.lstm(x, self.hidden)
         # out = out[:, :, :self.h_dim] + out[:, :, self.h_dim:]
-        
+
         out = self.l1(out)
         return out
