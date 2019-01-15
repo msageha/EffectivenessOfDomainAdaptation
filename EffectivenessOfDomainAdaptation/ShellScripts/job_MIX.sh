97e342c0bf51b30ca343c104e39130865841595d
Mizuki Sango <msageha@yahoo.co.jp>
2019-01-15 11:48:57 +0900

    edit shell script

--- EffectivenessOfDomainAdaptation/ShellScripts/job_MIX.sh
+++ EffectivenessOfDomainAdaptation/ShellScripts/job_MIX.sh
@@ -3,7 +3,7 @@
 # 実行時間を指定（5分）
 #$ -l h_rt=24:00:00
 # 名前（hill_climbing.e[ジョブ番号？]，hill_climbing.o[ジョブ番号？]というそれぞれエラー出力，標準出力ファイルが生成される．ただしこれの内容は信用できない）
-#$ -N MIX
+#$ -N MIX_Base
 # Module コマンドの初期化
 . /etc/profile.d/modules.sh
 module load cuda/9.0.176 cudnn/7.1 gnuplot
@@ -12,13 +12,13 @@ module load cuda/9.0.176 cudnn/7.1 gnuplot
 source /home/2/17M30683/.bash_profile
 cd /gs/hs0/tga-cl/sango-m-ab/research2/PAS_by_torch/EffectivenessOfDomainAdaptation
 #ga
-python3 train.py --emb_path '../../data/embedding/Word2VecWiki/entity_vector/entity_vector.model.txt' --gpu 0 --case ga --save --dump_dir './MIX/ga' --model MIX
-python3 test.py --gpu 0 --load_dir './MIX/ga'
+python3 train.py --emb_path '../../data/embedding/Word2VecWiki/entity_vector/entity_vector.model.txt' --gpu 0 --case ga --save --dump_dir './MIX_Base/ga' --model MIX
+python3 test.py --gpu 0 --load_dir './MIX_Base/ga'
 
 #o
-python3 train.py --emb_path '../../data/embedding/Word2VecWiki/entity_vector/entity_vector.model.txt' --gpu 0 --case o --save --dump_dir './MIX/o' --model MIX
-python3 test.py --gpu 0 --load_dir './MIX/o'
+python3 train.py --emb_path '../../data/embedding/Word2VecWiki/entity_vector/entity_vector.model.txt' --gpu 0 --case o --save --dump_dir './MIX_Base/o' --model MIX
+python3 test.py --gpu 0 --load_dir './MIX_Base/o'
 
 #ni
-python3 train.py --emb_path '../../data/embedding/Word2VecWiki/entity_vector/entity_vector.model.txt' --gpu 0 --case ni --save --dump_dir './MIX/ni' --model MIX
-python3 test.py --gpu 0 --load_dir './MIX/ni'
+python3 train.py --emb_path '../../data/embedding/Word2VecWiki/entity_vector/entity_vector.model.txt' --gpu 0 --case ni --save --dump_dir './MIX_Base/ni' --model MIX
+python3 test.py --gpu 0 --load_dir './MIX_Base/ni'
--- EffectivenessOfDomainAdaptation/ShellScripts/job_OutD2.sh
+++ EffectivenessOfDomainAdaptation/ShellScripts/job_OutD2.sh
@@ -12,13 +12,13 @@ module load cuda/9.0.176 cudnn/7.1 gnuplot
 source /home/2/17M30683/.bash_profile
 cd /gs/hs0/tga-cl/sango-m-ab/research2/PAS_by_torch/EffectivenessOfDomainAdaptation
 #out PB
-python3 train.py --emb_path '../../data/embedding/Word2VecWiki/entity_vector/entity_vector.model.txt' --gpu 0 --case ga --save --dump_dir './OutD/PB/ga' --model Base --media 'OC', 'OY', 'OW', 'PM', 'PN'
-python3 test.py --gpu 0 --load_dir './OutD/PB/ga'
+python3 train.py --emb_path '../../data/embedding/Word2VecWiki/entity_vector/entity_vector.model.txt' --gpu 0 --case o --save --dump_dir './OutD/PB/o' --model Base --media 'OC', 'OY', 'OW', 'PM', 'PN'
+python3 test.py --gpu 0 --load_dir './OutD/PB/o'
 
 #out PM
-python3 train.py --emb_path '../../data/embedding/Word2VecWiki/entity_vector/entity_vector.model.txt' --gpu 0 --case ga --save --dump_dir './OutD/PM/ga' --model Base --media 'OC', 'OY', 'OW', 'PB', 'PN'
-python3 test.py --gpu 0 --load_dir './OutD/PM/ga'
+python3 train.py --emb_path '../../data/embedding/Word2VecWiki/entity_vector/entity_vector.model.txt' --gpu 0 --case o --save --dump_dir './OutD/PM/o' --model Base --media 'OC', 'OY', 'OW', 'PB', 'PN'
+python3 test.py --gpu 0 --load_dir './OutD/PM/o'
 
 #out PN
-python3 train.py --emb_path '../../data/embedding/Word2VecWiki/entity_vector/entity_vector.model.txt' --gpu 0 --case ga --save --dump_dir './OutD/PN/ga' --model Base --media 'OC', 'OY', 'OW', 'PB', 'PM'
-python3 test.py --gpu 0 --load_dir './OutD/PN/ga'
+python3 train.py --emb_path '../../data/embedding/Word2VecWiki/entity_vector/entity_vector.model.txt' --gpu 0 --case o --save --dump_dir './OutD/PN/o' --model Base --media 'OC', 'OY', 'OW', 'PB', 'PM'
+python3 test.py --gpu 0 --load_dir './OutD/PN/o'
