66d534a19c63e028157ac9b6e7a86aac7382cbde
Mizuki Sango <msageha@yahoo.co.jp>
2019-01-16 19:29:25 +0900

    add unittest.py

---
+++ EffectivenessOfDomainAdaptation/run.sh
@@ -0,0 +1,17 @@
+#out OC
+python3 unittest.py --media OY OW PB PM PN
+
+#out OY
+python3 unittest.py --media OC OW PB PM PN
+
+#out OW
+python3 unittest.py OC OY PB PM PN
+
+#out PB
+python3 unittest.py --media OC OY OW PM PN
+
+#out PM
+python3 unittest.py --media OC OY OW PB PN
+
+#out PN
+python3 unittest.py --media OC OY OW PB PM
---
+++ EffectivenessOfDomainAdaptation/unittest.py
@@ -0,0 +1,34 @@
+import argparse
+
+import sys
+sys.path.append('../utils')
+from loader import DatasetLoading, load_model
+
+
+def create_arg_parser():
+    parser = create_arg_parser()
+    parser = argparse.ArgumentParser(description='main function parser')
+    parser.add_argument('--media', '-m', dest='media', nargs='+', type=str, default=['OC', 'OY', 'OW', 'PB', 'PM', 'PN'], choices=['OC', 'OY', 'OW', 'PB', 'PM', 'PN'], help='training media type')
+    return parser
+
+
+def main():
+    args = parser.parse_args()
+    emb_type = 'Word2VecWiki'
+    emb_path = '../../data/embedding/Word2VecWiki/entity_vector/entity_vector.model.txt'
+    dl = DatasetLoading(emb_type, emb_path, exo1_word='僕', exo2_word='おまえ', exoX_word='これ')
+    dl.making_intra_df()
+
+    trains_dict, vals_dict, tests_dict = dl.split_each_domain('intra')
+    for domain in args.media:
+        if domain not in trains_dict:
+            del trains_dict[domain]
+
+    args.__dict__['trains_size'] = sum([len(trains_dict[domain]) for domain in trains_dict.keys()])
+    args.__dict__['vals_size'] = sum([len(vals_dict[domain]) for domain in vals_dict.keys()])
+    args.__dict__['tests_size'] = sum([len(tests_dict[domain]) for domain in tests_dict.keys()])
+
+    print(f'training domain is {args.media}')
+    print(f'trains_size {args.__dict__["trains_size"]}')
+    print(f'vals_size {args.__dict__["vals_size"]}')
+    print(f'tests_size {args.__dict__["tests_size"]}')
