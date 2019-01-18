80596822d5d8e5980a572e4b0392209de59bf8ca
Mizuki Sango <msageha@yahoo.co.jp>
2019-01-18 22:30:46 +0900

    edit run script

--- EffectivenessOfDomainAdaptation/run.sh
+++ EffectivenessOfDomainAdaptation/run.sh
@@ -1,17 +1,17 @@
 #out OC
-python3 unittest.py --media OY OW PB PM PN
+python3 check.py --media OY OW PB PM PN
 
 #out OY
-python3 unittest.py --media OC OW PB PM PN
+python3 check.py --media OC OW PB PM PN
 
 #out OW
-python3 unittest.py OC OY PB PM PN
+python3 check.py OC OY PB PM PN
 
 #out PB
-python3 unittest.py --media OC OY OW PM PN
+python3 check.py --media OC OY OW PM PN
 
 #out PM
-python3 unittest.py --media OC OY OW PB PN
+python3 check.py --media OC OY OW PB PN
 
 #out PN
-python3 unittest.py --media OC OY OW PB PM
+python3 check.py --media OC OY OW PB PM
